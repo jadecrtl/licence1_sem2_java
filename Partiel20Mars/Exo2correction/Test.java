48a49b53878e23b7ae8c8022901453953d3c85f7
jadecrtl <jadecortial@gmail.com>
2021-05-12 16:00:10 +0200

    test du programme

--- Partiel20Mars/Exo2correction/Test.java
+++ Partiel20Mars/Exo2correction/Test.java
@@ -1,5 +1,11 @@
 public class Test {
     public static void main (String[] args) {
-        
+        LignePascal t;
+        t = new LignePascal();
+        for (int i=0; i<10; i++) {
+            System.out.println(t.toString());
+            t.buildNextLine();
+        }
+        System.out.println(t.toString());
     }
 }
